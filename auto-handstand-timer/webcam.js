1aa12b6037a194224d1d20b51d3ba2b9e557913d
Joshua Koo <zz85nus@gmail.com>
2023-12-22 23:04:35 -0800

    Clean up and modularize webcam code

---
+++ auto-handstand-timer/webcam.js
@@ -0,0 +1,49 @@
+// polls devices
+async function getConnectedDevices(type) {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return type ? devices.filter(device => device.kind === type) : devices;
+}
+
+// just a short hand for getUserMedia
+async function openMediaDevices(constraints) {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+}
+
+function setupVideoElement() {
+    const video = document.createElement('video');
+    video.autoplay = true;
+    video.playsInline = true;
+    video.controls = false;
+
+    document.body.appendChild(video);
+    return video;
+}
+
+// pipe 
+async function captureWebcamTo(dom, start) {
+    const constrains = {
+        //facingMode: 'user' // 'environmnet'
+        // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        'video': true,
+        'audio': false
+        // 'audio': { 'echoCancellation': true },
+        // 'video': {
+        //     'deviceId': cameraId,
+        //     'width': { 'min': minWidth },
+        //     'height': { 'min': minHeight }
+        // }
+    };
+
+    try {
+        const stream = await openMediaDevices(constrains);
+        console.log('Got MediaStream:', stream);
+
+        dom.srcObject = stream;
+        dom.onloadedmetadata = () => {
+            dom.play();
+            start && start();
+        };
+    } catch (error) {
+        console.error('Error accessing media devices.', error);
+    }
+}
