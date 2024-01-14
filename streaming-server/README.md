d8ae1dfcef906cf93078ce42ba3da798f9fe2ee0
Joshua Koo <zz85nus@gmail.com>
2024-01-14 00:11:58 -0800

    streaming-server: updated listen.html

--- streaming-server/README.md
+++ streaming-server/README.md
@@ -3,3 +3,9 @@
 
 Attempting to do real time streaming like websockets without really using
 websockets. So HTTP/2, SSE, or fetch streaming
+
+```
+npm run index.cjs
+```
+
+Then point your browser to https://localhost:3000/listen.html
--- streaming-server/index.cjs
+++ streaming-server/index.cjs
@@ -37,14 +37,95 @@ function getCerts() {
     }
 }
 
+class Streamer {
+    constructor() {
+        this.clients = new Set();
+
+        setInterval(() => {
+            this.broadcast({ time: Date.now() }, 'ping');
+        }, 5000);
+    }
+
+    add(req, res) {
+        var client = new Client(req, res);
+        res.on('close', () => {
+            this.clients.delete(client);
+            console.log(`closing client. Total: ${this.clients.size}`)
+        });
+
+        this.clients.add(client);
+
+        console.log(`new streaming client. Total: ${this.clients.size}`)
+
+        this.broadcast({ time: Date.now() }, 'ping');
+        this.broadcast({
+            clients: this.clients.size,
+        })
+
+        // res.write(`data: ${JSON.stringify({'hello': 'world'})}\n\n`);
+        // res.write(`data: ${JSON.stringify([1,2,3])}\n\n`);
+        // res.write(`data: ${JSON.stringify('yolo')}\n\n`);
+        // res.write(`data: 12345\n\n`);
+    }
+
+    broadcast(content, event) {
+        const json = JSON.stringify(content);
+        for (let client of this.clients) {
+            if (event) {
+                client.event(event, json);
+            } else {
+                client.send(json);
+            }
+        }
+    }
+}
+
+// streaming client
+class Client {
+    constructor(req, res) {
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        if (req.httpVersion !== '2.0') {
+            res.setHeader('Connection', 'keep-alive');
+        }
+        res.flushHeaders();
+
+        res.on('close', () => {
+            res.end();
+        });
+
+        this.res = res;
+    }
+
+    send(content) {
+        this.res.write(`data: ${content}\n\n`);
+    }
+
+    event(name, content) {
+        this.res.write(`event: ${name}\ndata: ${content}\n\n`);
+    }
+}
+
 const certs = getCerts();
+const streamer = new Streamer();
 
 const express = require('express');
 const http2Express = require('http2-express-bridge');
 const http2 = require('http2');
+const { Stream } = require('stream');
 const app = http2Express(express);
 
 app.use(express.static('public'))
+
+app.post('/send', (req) => {
+    console.log('POST', req);
+})
+
+app.get('/streaming', (req, res) => {
+    streamer.add(req, res);
+});
 
 const options = {
     key: certs.key,
