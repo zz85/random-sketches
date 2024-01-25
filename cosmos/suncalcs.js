00df1ab230c0e8f4e75b9b5711feae9ef9479d05
Joshua Koo <zz85nus@gmail.com>
2024-01-24 18:36:36 -0800

    suncalcs: display local time

--- cosmos/suncalcs.js
+++ cosmos/suncalcs.js
@@ -9,13 +9,41 @@ lon = -0.1;
 var now = new Date();
 
 var dayTimes1 = SunCalc1.getTimes(now, lat, lon);
-console.log('dayTimes1',  dayTimes1);
+// console.log('dayTimes1',  dayTimes1);
+console.log('# SunCalc1');
 
-console.log('sunrise', getLocalTime(dayTimes1.sunrise));
+[
+    'sunrise', 'sunset', 'nadir',
+    'goldenHour', 'goldenHourEnd'
+].forEach(k => {
+    console.log(k, getLocal(dayTimes1[k]));
+})
 
 var dayTimes3 = SunCalc3.getSunTimes(now, lat, lon);
-console.log('dayTimes3',  dayTimes3);
+// console.log('dayTimes3',  dayTimes3);
+
+console.log('# SunCalc3');
+
+['solarNoon', 'nadir', 'sunriseStart', 'sunriseEnd', 'sunsetStart', 'sunsetEnd'].forEach(k => {
+    console.log(k, getLocal(dayTimes3[k].value))
+})
+
+//blueHourDuskStart goldenHourDawnStart:
+
+function pad(str) {
+    const fmt = `00${str}`
+    return fmt.slice(fmt.length - 2)
+}
+
+function getLocal(date) {
+    return `${getLocalDate(date)} ${getLocalTime(date)}`
+}
 
 function getLocalTime(date) {
-    return `${date.getHours()}:${date.getMinutes()}`
+    return `${pad(date.getHours())}:${pad(date.getMinutes())}`
+    //  + `:${pad(date.getSeconds())}`
+}
+
+function getLocalDate(date) {
+    return `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDate())}`
 }
